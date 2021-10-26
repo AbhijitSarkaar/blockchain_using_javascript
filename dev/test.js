5de0a8982bc2f783ffcdea9ddcbac1f26974a728
AbhijitSarkaar <theabhijitsarkar@gmail.com>
2021-10-26 12:19:44 +0530

    create a new transaction

--- dev/test.js
+++ dev/test.js
@@ -2,7 +2,8 @@ const Blockchain = require('./blockchain')
 
 const bitcoin = new Blockchain()
 bitcoin.createNewBlock(1231, '8EWAE8AD', '9AFIS8W0')
-bitcoin.createNewBlock(4331, '4BQD86NSR', '439FWFTW75')
-bitcoin.createNewBlock(6543, '7UAIYRND', '1YWOJYTN')
+bitcoin.createNewTransaction(100, 'ALEX2SADFJ34', 'JEN23NJSANI4')
+bitcoin.createNewBlock(1232, '43ASDFI2', '1A32FAWE')
 
 console.log(bitcoin)
+// console.log(bitcoin.chain[bitcoin.chain.length - 1].transactions)
